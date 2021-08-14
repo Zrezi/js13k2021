3285e302dccf48d1aa8270c0c319846651d1d6ed
Zach Reznicek <reznicek@iastate.edu>
2021-08-14 16:00:22 -0500

    Add zip builder

--- finalize.js
+++ finalize.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const uglifyjs = require('uglify-js');
 const uglifycss = require('uglifycss');
+const admzip = require('adm-zip');
 
-fs.mkdirSync("game_min");
+if (!fs.existsSync("game_min")) {
+    fs.mkdirSync("game_min");
+}
 
 fs.copyFileSync("game/index.html", "game_min/index.html");
 
@@ -13,3 +16,9 @@ fs.writeFileSync("game_min/main.js", minifiedCode, "utf8");
 let css = fs.readFileSync("game/style.css", "utf8");
 let minifiedCSS = uglifycss.processString(css, {});
 fs.writeFileSync("game_min/style.css", minifiedCSS, "utf8");
+
+let zipFile = new admzip();
+zipFile.addLocalFolder("game_min");
+zipFile.writeZip("game.zip");
+
+fs.rmdirSync("game_min", { recursive: true });
